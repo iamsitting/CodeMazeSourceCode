3c233fdb5a0a496bff4be7179ee9b8d65565d0cd
iamsitting <cdsalamanca27@gmail.com>
2020-08-13 17:54:40 -0500

    initial commit

---
+++ FrontendProject/frontend-project/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  public forecasts: WeatherForecast[];
+
+  constructor(http: HttpClient) {
+    http.get<WeatherForecast[]>('https://localhost:5001/' + 'weatherforecast').subscribe(result => {
+      this.forecasts = result;
+    }, error => console.error(error));
+  }
+}
+
+interface WeatherForecast {
+  date: string;
+  temperatureC: number;
+  temperatureF: number;
+  summary: string;
+}
+
