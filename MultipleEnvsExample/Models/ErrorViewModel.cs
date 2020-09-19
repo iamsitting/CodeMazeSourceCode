0c8a79624f8cf95a435e57daa673e9fa2457601d
iamsitting <cdsalamanca27@gmail.com>
2020-09-19 13:11:45 -0500

    initial project

--- MultipleEnvsExample/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MultipleEnvsExample.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
