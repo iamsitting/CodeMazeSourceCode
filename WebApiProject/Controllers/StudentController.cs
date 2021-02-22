09b574727c9c2609aa7a957f900a05ff37c8bcb3
iamsitting <cdsalamanca27@gmail.com>
2021-02-22 13:32:34 -0600

    web api sample project

---
+++ WebApiProject/Controllers/StudentController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebApiProject.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StudentController : ControllerBase
+    {
+
+        private readonly ILogger<StudentController> _logger;
+
+        public StudentController(ILogger<StudentController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public Student Get()
+        {
+            return new Student()
+            {
+                Name = "John Doe",
+                Grade = 10,
+                LastLoggedIn = DateTime.Today,
+                Major = Major.Arts,
+                Enrolled = true
+            };
+        }
+    }
+}
