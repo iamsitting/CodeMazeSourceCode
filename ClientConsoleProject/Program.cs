d4585829558b8f102c97e9a628083849ff672038
iamsitting <cdsalamanca27@gmail.com>
2021-02-22 14:34:50 -0600

    add client console project

---
+++ ClientConsoleProject/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using System.Text.Json;
+namespace ClientConsoleProject
+{
+    public class Program
+    {
+        public static async Task Main()
+        {
+            using (var client = new HttpClient())
+            {
+                var student = await client.GetFromJsonAsync<Student>("https://localhost:5001/student");
+                var studentJson = JsonSerializer.Serialize<Student>(student);
+                Console.WriteLine($"{studentJson}");
+            }
+        }
+    }
+}
---
+++ ClientConsoleProject/Student.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Text.Json.Serialization;
+namespace ClientConsoleProject
+{
+    public class Student
+    {
+        [JsonPropertyName("FullName")]
+        public string Name {get; set;}
+        public int Grade {get; set;}
+        public DateTime LastLoggedIn {get; set;}
+        [JsonIgnore]
+        public Major Major {get; set;}
+        [JsonInclude]
+        public bool Enrolled;
+    }
+    public enum Major
+    {
+        Arts,
+        Sciences,
+        Philosophy
+    }
+}
